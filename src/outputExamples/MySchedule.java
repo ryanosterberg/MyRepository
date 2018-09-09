38f828ef0f3cdfe3c7333f1080d6ef88a41df041
Mr O <ryan.p.osterberg@gmail.com>
2018-09-09 12:39:32 -0500

    Student Schedule

---
+++ src/outputExamples/MySchedule.java
@@ -0,0 +1,13 @@
+package outputExamples;
+
+public class MySchedule {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Launch");
+		System.out.println("Data Structures");
+		System.out.println("Prep");
+		System.out.println("AP Java");
+	}
+
+}
