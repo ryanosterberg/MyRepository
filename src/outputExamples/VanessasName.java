8b7e992100efd8d32c4b000193fdfee7289015ad
Unknown <vanessa.osterberg@outlook.com>
2018-09-09 12:52:46 -0500

    Output Vanessa's Name

---
+++ src/outputExamples/VanessasName.java
@@ -0,0 +1,11 @@
+package outputExamples;
+
+public class VanessasName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Vanessa");
+	}
+
+}
